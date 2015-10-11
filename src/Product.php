463c2f4e56f20393d11fb4abcf16599a249a9e0a
Talib Allauddin <talib_570@live.com>
2015-10-12 03:06:12 +0500

    Added Product database table and model class

---
+++ src/Product.php
@@ -0,0 +1,31 @@
+<?php
+// src/Product.php
+
+use Doctrine\ORM\Mapping as ORM;
+
+class Product
+{
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
