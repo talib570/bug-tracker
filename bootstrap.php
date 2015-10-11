435b44e5780506b3426104c2e812651f65f44e53
Talib Allauddin <talib_570@live.com>
2015-10-12 03:00:44 +0500

    Added database configuration

--- bootstrap.php
+++ bootstrap.php
@@ -2,21 +2,25 @@
 // bootstrap.php
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\DriverManager;
 
 require_once "vendor/autoload.php";
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
 // or if you prefer yaml or XML
-//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
-//$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 
-// database configuration parameters
-$conn = array(
-    'driver' => 'pdo_sqlite',
-    'path' => __DIR__ . '/db.sqlite',
+/**
+ * $connectionParams     database connection parameters
+ * @var array         mysql://user:password@127.0.0.1:port/databasename
+ * Reference : http://stackoverflow.com/questions/6259424/troubleshooting-no-such-file-or-directory-when-running-php-app-console-doctri
+ * Reference : http://doctrine-dbal.readthedocs.org/en/latest/reference/configuration.html
+ */
+$connectionParams = array(
+    'url' => 'mysql://tahseent_digitem:LuyepBCPLJuTPzFe@127.0.0.1:8889/bug-tracker',
 );
+$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 
 // obtaining the entity manager
 $entityManager = EntityManager::create($conn, $config);
--- cli-config.php
+++ cli-config.php
@@ -1,5 +1,8 @@
 <?php
 // cli-config.php
+
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
 require_once "bootstrap.php";
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
